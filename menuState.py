4542759a3208ee32b890a9407e94cd371b215923
JGreyScales <77678089+JGreyScales@users.noreply.github.com>
2022-03-08 16:53:37 -0500

    a
    a

--- menuState.py
+++ menuState.py
@@ -6,6 +6,7 @@ class menuState:
         pass
 
     def menu(std, bitch = dict):
+        #std.border(3,110, 17, 128)
         for fuck in list(bitch.keys()):
             std.addstr(5 * (list(bitch.keys()).index(fuck) + 1), 119 - round(len(fuck)/2), fuck, curses.color_pair(bitch[fuck][0]))
         std.refresh()
